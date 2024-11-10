f262db0a96eec6c80f3b47bf0c938986efe7e5c4
alemoles <alemolerio@gmail.com>
2024-11-10 12:52:33 -0300

    Rock Paper Scissors | update UI style for button options

--- app/src/main/java/com/devstromo/rockpaperscissors/MainActivity.kt
+++ app/src/main/java/com/devstromo/rockpaperscissors/MainActivity.kt
@@ -2,6 +2,7 @@ package com.devstromo.rockpaperscissors
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.devstromo.rockpaperscissors.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,9 +14,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.stoneImageButton.setOnClickListener { playGame('s') }
-        binding.scissorsImageButton.setOnClickListener { playGame('z') }
-        binding.paperImageButton.setOnClickListener { playGame('p') }
+        binding.stoneImageButton.setOnClickListener { onButtonSelected(binding.stoneImageButton, 's') }
+        binding.paperImageButton.setOnClickListener { onButtonSelected(binding.paperImageButton, 'p') }
+        binding.scissorsImageButton.setOnClickListener { onButtonSelected(binding.scissorsImageButton, 'z') }
+    }
+
+    private fun onButtonSelected(selectedButton: ImageButton, playerChoice: Char) {
+        binding.stoneImageButton.isSelected = false
+        binding.paperImageButton.isSelected = false
+        binding.scissorsImageButton.isSelected = false
+
+        selectedButton.isSelected = true
+
+        playGame(playerChoice)
     }
 
     private fun playGame(playerChoice: Char) {
