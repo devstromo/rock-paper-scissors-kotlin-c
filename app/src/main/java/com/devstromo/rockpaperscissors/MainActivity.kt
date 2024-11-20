d9ef043e8530d2c642993cd5a59a9b6799ba3483
alemoles <alemolerio@gmail.com>
2024-11-19 21:24:20 -0300

    Rock Paper Scissors | add result cycle UI placeholder

--- app/src/main/java/com/devstromo/rockpaperscissors/MainActivity.kt
+++ app/src/main/java/com/devstromo/rockpaperscissors/MainActivity.kt
@@ -1,15 +1,37 @@
 package com.devstromo.rockpaperscissors
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.devstromo.rockpaperscissors.databinding.ActivityMainBinding
+import java.util.Random
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     private var playerOption: Char? = null
+
+    private var iconImageView: ImageView? = null
+    private lateinit var stopButton: Button
+
+    // Array of icons (drawable resources)
+    private val icons = intArrayOf(
+        R.drawable.ic_stone,
+        R.drawable.ic_paper,
+        R.drawable.ic_scissor,
+    )
+
+    private lateinit var handler: Handler
+    private var iconSwitcher: Runnable? = null
+    private var random: Random? = null
+    private var isRunning = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +56,19 @@ class MainActivity : AppCompatActivity() {
                 'z'
             )
         }
+
+        iconImageView = findViewById(R.id.iconImageView)
+        stopButton = findViewById(R.id.stopButton)
+
+        handler = Handler(Looper.getMainLooper())
+        random = Random()
+        isRunning = true
+
+
+        startCyclingIcons()
+
+
+        stopButton.setOnClickListener { stopCyclingIcons() }
     }
 
     private fun onButtonSelected(selectedButton: ImageButton, playerChoice: Char) {
@@ -54,6 +89,27 @@ class MainActivity : AppCompatActivity() {
 
     private fun setPlayerOptionSelection(playerChoice: Char) {
         playerOption = playerChoice
+    }
+
+    private fun startCyclingIcons() {
+        iconSwitcher = object : Runnable {
+            override fun run() {
+                val randomIndex = random!!.nextInt(icons.size)
+                iconImageView!!.setImageResource(icons[randomIndex])
+
+                if (isRunning) {
+                    handler.postDelayed(this, 100)
+                }
+            }
+        }
+
+        handler.post(iconSwitcher as Runnable)
+    }
+
+    private fun stopCyclingIcons() {
+        isRunning = false // Stop cycling
+        iconSwitcher?.let { handler.removeCallbacks(it) } // Stop the handler
+        // You can add further actions here, e.g., save the result or start a new level
     }
 
     external fun playGameJNI(playerChoice: Char): String
